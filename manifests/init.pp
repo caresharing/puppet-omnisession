cd5e3c8752d02b1c1aca7f75e536d8d1d541891d
Jan Mendoza <poymode@gmail.com>
2015-11-23 17:33:21 +0800

    Initial working commit

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,48 +1,47 @@
-# Class: omnisession
-# ===========================
-#
-# Full description of class omnisession here.
-#
-# Parameters
-# ----------
-#
-# Document parameters here.
-#
-# * `sample parameter`
-# Explanation of what this parameter affects and what it defaults to.
-# e.g. "Specify one or more upstream ntp servers as an array."
-#
-# Variables
-# ----------
-#
-# Here you should define a list of variables that this module would require.
-#
-# * `sample variable`
-#  Explanation of how this variable affects the function of this class and if
-#  it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#  External Node Classifier as a comma separated list of hostnames." (Note,
-#  global variables should be avoided in favor of class parameters as
-#  of Puppet 2.6.)
-#
-# Examples
-# --------
-#
-# @example
-#    class { 'omnisession':
-#      servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
-#    }
-#
-# Authors
-# -------
-#
-# Author Name <author@domain.com>
-#
-# Copyright
-# ---------
-#
-# Copyright 2015 Your name here, unless otherwise noted.
-#
-class omnisession {
+class omnisession (
+  $gem_version  = $::omnisession::params::gem_version,
+  $gem_source   = $::omnisession::params::gem_source,
+  $redis_url    = $::omnisession::params::redis_url,
+  $bind_address = $::omnisession::params::bind_address,
+  $port         = $::omnisession::params::port,
+  $count        = $::omnisession::params::count,
+  $log_file     = $::omnisession::params::log_file,
+  $pid_file     = $::omnisession::params::pid_file,
+  $control_port = $::omnisession::params::control_port,
+  $runuser      = $::omnisession::params::runuser,
+  $gem_bin      = $::omnisession::params::gem_bin,
+  $config_file  = $::omnisession::params::config_file,
+  $lock_file    = $::omnisession::params::lock_file,
+  $initscript   = $::omnisession::params::initscript,
 
+) inherits ::omnisession::params {
 
+  package { "palmade-omnisession":
+    ensure   => $gem_version,
+    provider => 'gem',
+    source   => $gem_source,
+  }
+
+  file { $config_file:
+    ensure  => file,
+    owner   => $runuser,
+    content => epp('omnisession/omnisession.yml.epp'),
+    require => Package['palmade-omnisession'],
+  }
+
+  file { $initscript:
+    ensure  => present,
+    owner   => root,
+    content => epp('omnisession/omnisessiond.init'),
+    require => File[$config_file],
+    notify  => Service['omnisessiond'],
+  }
+
+  service { "omnisessiond":
+    ensure => running,
+    enable => true,
+  }
 }
+
+
+
